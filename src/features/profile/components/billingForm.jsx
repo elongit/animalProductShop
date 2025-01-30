13e53eb339fd2f7535ad5f7655f3df51629a235c
ossama <ossama@gmail.com>
2025-01-30 14:15:20 -0600

    added new billingForm for user

--- src/features/profile/components/billingForm.jsx
+++ src/features/profile/components/billingForm.jsx
@@ -1,4 +1,57 @@
+import { useState  , useEffect} from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectPaymentData } from "../../payment/checkoutSlice";
+import { useNavigate } from "react-router-dom";
 export default function BillingForm() {
+  const data = useSelector(selectPaymentData)
+  const navigate = useNavigate()
+  const { id } = useParams();
+  console.log(id);
+  
+  const billingData = data.find((billing) => billing.userId === id);
+  const [formData, setFormData] = useState({
+    billingName: "",
+    address: '',
+    city: '',
+    zipCode: '',
+    ccv: '',
+    phone: '',
+    expiryDate: '',
+  });
+ 
+  
+
+
+  useEffect(() => {
+    if (billingData ) {
+      // If billingData  is found, set the formData
+      setFormData({
+        id : billingData.id,
+        billingName: billingData.cardName,
+        address: billingData.address,
+        city: billingData.city,
+        zipCode: billingData.zip,
+        ccv: billingData.ccv,
+        phone: billingData.tel,
+        expiryDate : billingData.expiryDate,
+      });
+    }
+    
+  }, [billingData , navigate]);
+  
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+
+    });
+  }
+
+
     return (
       <fieldset className="w-full p-0 md:p-5">
         <legend className="text-xl md:text-2xl mb-5 font-bold text-primary-color">
@@ -12,38 +65,64 @@ export default function BillingForm() {
           <input
             type="text"
             name="billingName"
+            value={formData.billingName}
             placeholder="Billing Name"
             className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color"
+            onChange={handleChange}
           />
           <input
             type="text"
             name="address"
+            value={formData.address}
             placeholder="Street Address"
             className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color"
+            onChange={handleChange}
           />
           <input
             type="text"
             name="city"
             placeholder="City"
+            value={formData.city}
             className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color"
+            onChange={handleChange}
           />
           <input
             type="text"
             name="zipCode"
             placeholder="ZIP Code"
+            value={formData.zipCode}
             className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color"
+            onChange={handleChange}
           />
           <input
             type="text"
-            name="country"
-            placeholder="Country"
+            name="expiryDate"
+            id="expiryDate"
+            placeholder="Expiry date"
+            value={formData.expiryDate}
+
             className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color"
+            onChange={handleChange}
+          />
+
+          <input
+            type="number"
+            name="ccv"
+            id="ccv"
+            placeholder="ccv"
+            value={formData.ccv}
+            min={100}
+            max={999}
+            className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color"
+            onChange={handleChange}
           />
           <input
             type="tel"
             name="phone"
             placeholder="Phone Number"
-            className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color"
+            value={formData.phone}
+            className="p-3 outline-none w-full ring-0 bg-slate-100 focus:ring-4 rounded focus:ring-primary-color col-span-2"
+            onChange={handleChange}
           />
           <button
             type="submit"
