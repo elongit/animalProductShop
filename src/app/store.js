1fc95d946dfdad0f7c2a33fb8c967727674461e7
ossama <ossama@gmail.com>
2025-01-30 14:10:01 -0600

    added new reducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import CartReducer from '../features/cart/cartSlice'
 import ProductReducer from '../features/product/productSlice'
 import  AuthReducer from '../features/auth/authSlice'
 import OrderReducer from '../features/orders/orderSlice'
+import CheckOutReducer from '../features/payment/checkoutSlice'
 export const store = configureStore({
     reducer:{
         shoppingCart : CartReducer,
         products : ProductReducer,
         auth : AuthReducer,
-        orders : OrderReducer
+        orders : OrderReducer,
+        checkout : CheckOutReducer
     }
 })
